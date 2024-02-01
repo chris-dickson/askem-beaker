af59713fcab85842cc2ff8572c37b9ffcc50a850
Tom Szendrey <T.Szendrey@hotmail.com>
2024-02-01 15:40:10 -0500

    more imports on setup

--- src/askem_beaker/contexts/mira_model_edit/procedures/python3/setup.py
+++ src/askem_beaker/contexts/mira_model_edit/procedures/python3/setup.py
@@ -2,3 +2,5 @@ import requests; import pandas as pd; import numpy as np; import scipy;
 import json; import mira;
 import sympy; import itertools; from mira.metamodel import *; from mira.modeling import Model;
 from mira.sources.amr import model_from_json; from mira.modeling.viz import GraphicalModel;
+from mira.metamodel.template_model import Parameter, Distribution , Observable, \
+    Initial, Concept, TemplateModel
