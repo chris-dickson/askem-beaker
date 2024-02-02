1ddb70dc353771e40df54e9d1eade47164344fa6
Tom Szendrey <T.Szendrey@hotmail.com>
2024-02-02 15:47:39 -0500

    controlled deg

--- src/askem_beaker/contexts/mira_model_edit/procedures/python3/add_controlled_degradation_template.py
+++ src/askem_beaker/contexts/mira_model_edit/procedures/python3/add_controlled_degradation_template.py
@@ -1,4 +1,5 @@
 subject_concept = Concept(name = "{{ subject_name|default('subject_name') }}")
+controller_concept = Concept(name = "{{controller_name|default('controller_name')}}")
 parameter_unit = Unit(expression = sympy.Symbol("{{ parameter_units|default('parameter_units')}}"))
 
 parameters = {
@@ -6,12 +7,14 @@ parameters = {
 }
 
 initials = { 
-    "{{subject_name|default('subject_name')}}": Initial(concept = subject_concept, expression = sympy.Float({{subject_initial_value|default(1)}}))
+    "{{subject_name|default('subject_name')}}": Initial(concept = subject_concept, expression = sympy.Float({{subject_initial_value|default(1)}})),
+    "{{controller_name|default('controller_name')}}": Initial(concept = controller_concept, expression = sympy.Float({{controller_concept|default(1)}}))
 }
 
 model = model.add_template(
-    template = NaturalDegradation(
+    template = ControlledDegradation(
         subject = subject_concept,
+        controller = controller_concept,
         rate_law = sympy.parsing.sympy_parser.parse_expr("{{ template_expression|default('EXPRESSION_HERE') }}"),
         name = "{{ template_name|default('template_name') }}"
     ),
