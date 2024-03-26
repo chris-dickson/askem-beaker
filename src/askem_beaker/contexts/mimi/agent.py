954636121b0bba2fb106042c580bc4a44745258b
Five Grant <5@fivegrant.com>
2024-03-26 16:09:48 -0500

    Update Mimi for eval (#126)
    * Move all Mimi tools to toolset
    
    * Begin function grab fix
    
    * Include info change
    
    * Lookup functions again

--- src/askem_beaker/contexts/mimi/agent.py
+++ src/askem_beaker/contexts/mimi/agent.py
@@ -13,7 +13,7 @@ from typing import List
 class Toolset:
     """Toolset for our context"""
 
-    @tool(autosummarize=True)
+#     @tool(autosummarize=True)
     async def search_installed_packages(self, name: str, agent: AgentRef) -> str:
         """
         Search installed packages using a naive match
@@ -72,6 +72,63 @@ class Toolset:
             parent_header={},
         )
         return response["return"]
+
+
+    @tool(autosummarize=True)
+    async def retrieve_documentation_for_module(self, package_name: str, agent: AgentRef) -> str:
+        """
+        Gets the specified module documentation
+
+        Args:
+            package_name (str): this is the name of the package to get information about.
+        Returns:
+            str: Markdown of the module docs
+        """
+        code = agent.context.get_code("get_module_docs", {"module": package_name})
+        response = await agent.context.beaker_kernel.evaluate(
+            code,
+            parent_header={},
+        )
+        return response["return"]
+
+
+    @tool(autosummarize=True)
+    async def get_function_docstring(self, function_name: str, agent: AgentRef):
+        """
+        Use this tool to additional information on individual function such as their inputs, outputs and descrption (and generally anything else that would be in a docstring)
+        
+        Read the information returned to learn how to use the function and which arguments they take.
+        
+        The function names used in the input to this tool should include the entire module hierarchy
+
+        If this fails, this means the function does not exist.
+        
+        Args:
+            function_name (str): name of the function to lookup. 
+        """
+        code = f"""
+            import DisplayAs, JSON3
+            try {function_name} 
+            catch 
+                DisplayAs.unlimited(
+                    JSON3.write(
+                        Dict("docs" => "{function_name} not defined")
+                    )
+                )
+            else
+                docstring = string(@doc({function_name}))
+                doc_object = Dict("docs" => docstring)
+                DisplayAs.unlimited(
+                    JSON3.write(doc_object)
+                )
+            end
+        """
+        response = await agent.context.beaker_kernel.evaluate(
+            code,
+            parent_header={},
+        )
+        docs = response["return"]["docs"]
+        return docs
 
 
 class Agent(NewBaseAgent):
@@ -142,83 +199,4 @@ class Agent(NewBaseAgent):
         """
         code = f"Mimi.plot({model_name}, :{component_name}, :{variable_name})"
         return code
-
-
-    @tool(autosummarize=True)
-    async def retrieve_documentation_for_module(self, package_name: str) -> str:
-        """
-        Gets the specified module documentation
-
-        Args:
-            package_name (str): this is the name of the package to get information about.
-        Returns:
-            str: Markdown of the module docs
-        """
-        code = self.context.get_code("get_module_docs", {"module": package_name})
-        response = await self.context.beaker_kernel.evaluate(
-            code,
-            parent_header={},
-        )
-        return response["return"]["documentation"]
-    
-
-
-    @tool(autosummarize=True)
-    async def get_available_functions(self, package_name: str, agent: AgentRef):
-        """
-        Querying against the module or package should list all exprted functions in that module, so you can use this to discover available
-        functions and the query the function to get usage information.
-        
-        This function should be used to discover the available functions in the target library or module and get an object containing their docstrings so you can figure out how to use them.
-
-        Read the docstrings to learn how to use the functions and which arguments they take.
-
-        Args:
-            package_name (str): this is the name of the package to get the function docks from.
-        """
-        functions = {}
-
-        code = self.agent.context.get_code("info", {"module": package_name})
-        response = await self.agent.beaker_kernel.evaluate(
-            code,
-            parent_header={},
-        )
-        functions = response["return"]
-        print(f"Fetched func info")
-        agent.context.functions.update(functions)
-        help_string=''
-        for name, help_text in functions.items():
-            help_string+=f'{name}: {help_text}'
-            agent.context.functions[name]=help_text
-        return help_string
-
-
-    @tool(autosummarize=True)
-    async def get_functions_docstring(self, list_of_function_names: list, agent: AgentRef):
-        """
-        Use this tool to additional information on individual function such as their inputs, outputs and description (and generally anything else that would be in a docstring)
-        You should ALWAYS use this tool before writing or checking code to check the function signatures of the functions you are about to use.
-        
-        Read the information returned to learn how to use the function and which arguments they take.
-        
-        The function names used in the input to this tool should include the entire module hierarchy
-        
-        Args:
-            list_of_function_names (list): this is a list of the the names of the functions and/or classes to get information about. 
-        """
-        #TODO: figure out cause of this and remove ugly filter
-        if type(list_of_function_names)==dict:
-            list_of_function_names=list_of_function_names['list_of_function_names']
-
-        code = self.agent.get_code("info", {"from_module": "false", "function_names": ",".join(list_of_function_names)})
-        response = await self.agent.context.beaker_kernel.evaluate(
-            code,
-            parent_header={},
-        )
-        functions = response["return"]
-        help_string=''
-        for name, help_text in functions.items():
-            help_string+=f'{name}: {help_text}'
-            agent.context.functions[name]=help_text
-        return help_string
 
--- src/askem_beaker/contexts/mimi/procedures/julia/info.jl
+++ src/askem_beaker/contexts/mimi/procedures/julia/info.jl
@@ -4,25 +4,25 @@ _IGNORED_SYMBOLS = [:Base, :Core, :InteractiveUtils, :Main]
 _from_module = {{from_module | default("true") }}
 _return_source = {{ return_source | default("false") }} # TODO: Use this with CodeTracking.jl
 _selected_module = :{{ module | default("nothing") }}
-_chosen_funcs_str = {{ function_names | default("[]") }}
+_chosen_funcs_str = "{{ function_names | default("") }}"
 
 
 _function_names = 
     if _from_module
         _module_names = 
-            if !isnothing(selected_module)
+            if !isnothing(_selected_module)
                 filter(x -> in(x, _IGNORED_SYMBOLS) && isa(x, Module), names(Main))
             else
-                _selected_module
+                [_selected_module]
             end
-        filter(!in(_modules_names), reduce(vcat, [names(mod) for mod in _module_names]))
+        filter(!in(_module_names), reduce(vcat, [names(eval(mod); all=true) for mod in _module_names]; init=[]))
     else
-        split(_chosen_func_str, ",")
+        Symbol.(split(_chosen_funcs_str, ","))
     end
 
 _docs = Dict{Symbol, Any}()
 for func in _function_names
-    _docs[func] = eval(:(@doc $func))
+    _docs[func] = string(eval(:(@doc $func)))
 end
 
 _docs |> DisplayAs.unlimited ∘ JSON3.write
